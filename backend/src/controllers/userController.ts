95f5b248eae00583507d72d31936a69a14d64b33
FaizanAanwar2801 <iamanwarfaizan@gmail.com>
2024-08-31 02:42:15 +0530

    Adding user controller(modified)

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -1,21 +1,53 @@
 import { PrismaClient } from "@prisma/client/extension";
 import { withAccelerate } from "@prisma/extension-accelerate";
 import { Context } from "hono";
+import { sign } from "hono/jwt";
 
 enum StatusCode {
     BADREQ = 400,
     NOTFOUND = 404,
-    NOTPERMISSIOON = 403,
+    NOTPERMISSION = 403,
 }
 
 export async function signup(c: Context) {
     const prisma = new PrismaClient({
         datasourceUrl: c.env.DATABASE_URL,
     }).$extends(withAccelerate());
+
+    const body = await c.req.json();
+    try{
+        const user = prisma.user.create({
+            data:{
+                email: body.email,
+                password: body.password
+            }
+        });
+        const jwt = await sign({id: user.id},c.env.JWT_SECRET);
+        return c.json({jwt});
+    }
+    catch(e){
+        c.status(StatusCode.NOTPERMISSION)
+        return c.json({error:"eroor while signing up"});
+    }
 }
 
 export async function signin(c: Context) {
     const prisma = new PrismaClient({
         datasourceUrl: c.env.DATABASE_URL,
     }).$extends(withAccelerate());
+
+    const body = await c.req.json();
+	const user = await prisma.user.findUnique({
+		where: {
+			email: body.email
+		}
+	});
+
+	if (!user) {
+		c.status(StatusCode.NOTFOUND);
+		return c.json({ error: "user not found" });
+	}
+
+	const jwt = await sign({ id: user.id }, c.env.JWT_SECRET);
+	return c.json({ jwt });
 }
