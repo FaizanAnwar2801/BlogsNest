bae7eefd8c7b63eb5d83600612bdccd3b5452060
FaizanAanwar2801 <iamanwarfaizan@gmail.com>
2024-09-10 13:05:26 +0530

    Adding main routes(With Bindings.)

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,7 +3,13 @@ import { cors } from 'hono/cors'
 import { userRouter } from './routes/userRouter';
 import { blogRouter } from './routes/blogsRouter';
 
-const app = new Hono()
+const app = new Hono<{
+    Bindings: {
+        DATABASE_URL: string;
+        JWT_SECRET: string;
+    }
+}>();
+
 app.use(cors());
 
 app.route('api/v1/user', userRouter)
