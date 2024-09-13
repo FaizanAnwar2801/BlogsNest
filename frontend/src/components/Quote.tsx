b172b8da129c744e8ae024de0a7c614c4e3216fb
FaizanAanwar2801 <iamanwarfaizan@gmail.com>
2024-09-13 21:41:52 +0530

    Adding Quote Component.

---
+++ frontend/src/components/Quote.tsx
@@ -0,0 +1,19 @@
+
+export const Quote = () => {
+    return <div className=" flex justify-center flex-col bg-gray-100 h-screen ">
+        <div className="flex justify-center">
+            <div className="max-w-lg">
+                <div className="text-2xl font-bold">
+                    "The customer support I received was exceptional. The support team went above and beyond to address my concerns"
+                </div>
+                <div className="max-w-md text-lg font-semibold text-left mt-4">
+                    Julies Winfield
+                </div>
+                <div className="max-w-md text-sm font-light text-gray-500">
+                    CEO, Acme corp
+                </div>
+            </div>
+        </div>
+        
+    </div>
+}
