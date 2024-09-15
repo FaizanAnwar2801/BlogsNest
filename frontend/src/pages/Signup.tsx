97b1d6290b5abe1ef6064faf47b0bcbceae3c30c
FaizanAanwar2801 <iamanwarfaizan@gmail.com>
2024-09-16 00:00:30 +0530

    Adding Sign-up page(Modified)

--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -5,10 +5,10 @@ export const Signup = () => {
     return <div>
         <div className="grid grid-cols-1 lg:grid-cols-2">
             <div>
-                <Auth/>
+                <Auth type="signup" />
             </div>
             <div className="hidden lg:block">
-                <Quote/>
+                <Quote />
             </div>
         </div>
     </div>
