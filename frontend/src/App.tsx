8d91ab42123176618e5f87d4921386e2d3a1ea58
FaizanAanwar2801 <iamanwarfaizan@gmail.com>
2024-09-17 19:35:48 +0530

    Adding Main App File(Modified)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Signup } from './pages/Signup'
 import { Signin } from './pages/Signin'
 import { Blog } from './pages/Blog'
+import { Landing } from './pages/Landing'
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
+        <Route path="/" element={<Landing />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/signin" element={<Signin />} />
           <Route path="/blog/:id" element={<Blog />} />
