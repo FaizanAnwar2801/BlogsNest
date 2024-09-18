300d272e9b3d185b42ce773e7e5389e4ea3c2ec5
FaizanAanwar2801 <iamanwarfaizan@gmail.com>
2024-09-18 23:53:54 +0530

    Adding Auth Component (Backend URL.)

--- frontend/src/components/Auth.tsx
+++ frontend/src/components/Auth.tsx
@@ -3,6 +3,8 @@ import { Link, useNavigate } from "react-router-dom";
 import { SignupInput } from "@faizancodes2808/blogsnest-common";
 import axios from "axios";
 
+import { BACKEND_URL } from "../config";
+
 export const Auth = ({ type }: { type: "signup" | "signin" }) => {
     const navigate = useNavigate();
     const [postInputs, setPostInputs] = useState<SignupInput>({
