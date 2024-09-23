6952d9ef3b467b22a312800393783e5c2f69b930
FaizanAanwar2801 <iamanwarfaizan@gmail.com>
2024-09-23 21:23:37 +0530

    Adding Blog (by blog id) Page.

--- frontend/src/pages/Blog.tsx
+++ frontend/src/pages/Blog.tsx
@@ -1,7 +1,31 @@
+// atomFamaily or selectorFamily
+import { useBlog } from "../hooks"
+import { Appbar } from "../components/Appbar"
+import { BlogSkeleton } from "../components/BlogSkeleton"
+import { useParams } from "react-router-dom"
+import { FullBlog } from "../components/FullBlog";
+
+
 export const Blog = () => {
-    return(
-    <div>
-        Blog
-    </div>
+    const { id } = useParams<string>();
+    const { blog, loading } = useBlog({
+        id: id || ""
+    })
+
+    if (loading) {
+        return <div>
+            <Appbar />
+            <div className="flex justify-center">
+                <div>
+                    <BlogSkeleton />
+                </div>
+            </div>
+        </div>
+    }
+
+    return (
+        <div>
+            <FullBlog blog = {blog}/>
+        </div>
     )
 }
