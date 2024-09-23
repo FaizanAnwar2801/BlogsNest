36904ce5be0e70b9822eff9ebc1daefbbc8b9559
FaizanAanwar2801 <iamanwarfaizan@gmail.com>
2024-09-24 00:50:39 +0530

    Adding Appbar component(Avatar Modified.)

--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -1,5 +1,6 @@
-import { Avatar } from "./BlogsCard"
+// import { Avatar } from "./BlogsCard"
 import { Link } from "react-router-dom"
+import DropdownButton from "./Dropdown"
 
 export const Appbar = () => {
     return <div className="border-b flex justify-between px-10 py-4">
@@ -10,8 +11,8 @@ export const Appbar = () => {
             <Link to={`/add-new-blog`}>
                 <button type="button" className="mr-4 text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 ">New</button>
             </Link>
-
-            <Avatar size={"big"} name="Faizan" />
+            <DropdownButton/>
+            {/* <Avatar size={"big"} name="Faizan" /> */}
         </div>
     </div>
 }
