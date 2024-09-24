d4537a1931812cea299481b97271c066647c5a63
FaizanAanwar2801 <iamanwarfaizan@gmail.com>
2024-09-24 19:21:58 +0530

    Adding User Blogs Page.

---
+++ frontend/src/pages/UserBlog.tsx
@@ -0,0 +1,38 @@
+import { Appbar } from "../components/Appbar"
+import { BlogCard } from "../components/BlogsCard"
+import { BlogSkeleton } from "../components/BlogSkeleton";
+import { useMyBlogs } from "../hooks";
+
+export const UserBlog = () => {
+    const { loading, userBlogs } = useMyBlogs();
+
+    if (loading) {
+        return <div>
+            <Appbar /> 
+            <div  className="flex justify-center">
+                <div>
+                    <BlogSkeleton />
+                    <BlogSkeleton />
+                    <BlogSkeleton />
+                    <BlogSkeleton />
+                    <BlogSkeleton />
+                </div>
+            </div>
+        </div>
+    }
+
+    return <div>
+        <Appbar />
+        <div  className="flex justify-center">
+            <div>
+                {userBlogs.map(blog => <BlogCard 
+                key= {blog.id}
+                authorName={blog.author.name} 
+                title={blog.title} 
+                content={blog.content} 
+                publishedDate={"22nd September"} 
+                id={blog.id}/>)}
+            </div>
+        </div>
+    </div>
+}
