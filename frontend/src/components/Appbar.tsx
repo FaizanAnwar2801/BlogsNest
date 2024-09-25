0d68501c443f53b7a20f6b18392f2fc848b3c1ef
FaizanAanwar2801 <iamanwarfaizan@gmail.com>
2024-09-25 23:37:18 +0530

    Adding Appbar component(redundent comments removed.)

--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -1,4 +1,3 @@
-// import { Avatar } from "./BlogsCard"
 import { Link } from "react-router-dom"
 import DropdownButton from "./Dropdown"
 
@@ -12,7 +11,6 @@ export const Appbar = () => {
                 <button type="button" className="mr-4 text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 ">New</button>
             </Link>
             <DropdownButton/>
-            {/* <Avatar size={"big"} name="Faizan" /> */}
         </div>
     </div>
 }
