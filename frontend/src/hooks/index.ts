13726ea53256694767beebef6cb9301739b18eda
FaizanAanwar2801 <iamanwarfaizan@gmail.com>
2024-09-25 20:02:31 +0530

    Adding custom hoooks.

---
+++ frontend/src/hooks/index.ts
@@ -0,0 +1,95 @@
+import { useEffect, useState } from "react";
+import { BACKEND_URL } from "../config";
+import axios from "axios";
+
+export interface Blog {
+    "id": string,
+    "title": string,
+    "content": string,
+    "author": {
+        "name": string,
+        "id":string
+    }
+}
+
+
+export const useBlog = ({ id }: { id: string }) => {
+
+    const [loading, setLoading] = useState(true);
+    const [blog, setBlog] = useState<Blog>();
+
+    useEffect(() => {
+        try {
+            axios.get(`${BACKEND_URL}/api/v1/blog/get-blog/${id}`, {
+                headers: {
+                    Authorization: localStorage.getItem("token")
+                }
+            }).then(response => {
+                setBlog(response.data.blog)
+                setLoading(false)
+            })
+        } catch (e) {
+            console.error('Error fetching data:', e);
+        }
+
+    }, [id])
+
+    return {
+        loading,
+        blog
+    }
+}
+
+
+export const useBlogs = () => {
+    const [loading, setLoading] = useState(true);
+    const [allBlog, setBlogs] = useState<Blog[]>([])
+
+    useEffect(() => {
+        try {
+            axios.get(`${BACKEND_URL}/api/v1/blog/get-all-blog`, {
+                headers: {
+                    Authorization: localStorage.getItem("token")
+                }
+            }).then(response => {
+                setBlogs(response.data.allBlog)
+                setLoading(false)
+            })
+        } catch (e) {
+            console.error('Error fetching data:', e);
+        }
+
+    }, [])
+
+    return {
+        loading,
+        allBlog
+    }
+}
+
+export const useMyBlogs = () => {
+    const [loading, setLoading] = useState(true);
+    const [userBlogs, setuserBlogs] = useState<Blog[]>([])
+
+    useEffect(() => {
+        try {
+            axios.get(`${BACKEND_URL}/api/v1/blog/user-blog`, {
+                headers: {
+                    Authorization: localStorage.getItem("token")
+                }
+            }).then(response => {
+                setuserBlogs(response.data.userBlog)
+                setLoading(false)
+            })
+        } catch (e) {
+            console.error('Error fetching data:', e);
+        }
+
+    }, [])
+
+    return {
+        loading,
+        userBlogs
+    }
+}
+
