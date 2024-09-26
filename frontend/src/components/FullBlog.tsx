0eed0ecf611d8b1a6201bde1113ea5b6117c66dd
FaizanAanwar2801 <iamanwarfaizan@gmail.com>
2024-09-26 18:11:52 +0530

    Adding single Blog render(div Conditioned.)

--- frontend/src/components/FullBlog.tsx
+++ frontend/src/components/FullBlog.tsx
@@ -8,6 +8,23 @@ import axios from "axios"
 export const FullBlog = ({ blog }: { blog: Blog }) => {
     const navigate = useNavigate();
 
+    function getUserId() {
+        const token = localStorage.getItem("token")
+        if (!token) return null
+
+        try {
+            const tokenParts = token.split('.');
+            if (tokenParts.length !== 3) return null
+            const payload = tokenParts[1];
+            const Decoded = JSON.parse(atob(payload))
+
+            return Decoded.id || null;
+        } catch (e) {
+            console.log("failed to parse token.", e)
+            return null
+        }
+    }
+
     async function onDelete() {
         const useConfirmed = window.confirm("Delete Post.")
         if (useConfirmed) {
@@ -22,6 +39,10 @@ export const FullBlog = ({ blog }: { blog: Blog }) => {
             navigate(`/user-blogs`);
         }
     }
+
+    const authorVal = blog.author.id;
+    const userVal = getUserId() || "";
+    console.log(userVal)
 
 
     return <div>
@@ -54,19 +75,20 @@ export const FullBlog = ({ blog }: { blog: Blog }) => {
                             <div className="flex justify-between pt-2 text-m text-slate-500">
                                 Random catch phrase about the author's ability to grab the user's attention
                             </div>
-                            <div className="flex justify-items-center pt-10">
-                                <Link to={`/edit-blog/${blog.id}`}>
-                                    <button type="button"
-                                        className="mr-4 text-white bg-green-700 hover:bg-green-800 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 ">
-                                        Edit
-                                    </button>
-                                </Link>
-                                <button onClick={onDelete} className="mr-4 text-white bg-rose-600 hover:bg-red-700 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 ">Delete</button>
-                            </div>
+                            {authorVal === userVal && (
+                                <div className="flex justify-items-center pt-10">
+                                    <Link to={`/edit-blog/${blog.id}`}>
+                                        <button type="button"
+                                            className="mr-4 text-white bg-green-700 hover:bg-green-800 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 ">
+                                            Edit
+                                        </button>
+                                    </Link>
+                                    <button onClick={onDelete} className="mr-4 text-white bg-rose-600 hover:bg-red-700 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 ">Delete</button>
+                                </div>
+                            )}
                         </div>
                     </div>
                 </div>
-
             </div>
         </div>
     </div>
